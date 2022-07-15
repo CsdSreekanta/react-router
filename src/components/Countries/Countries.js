b44c7b1339445392d2715460712fea578f09da3b
HP <sreekanta.csd@gmail.com>
2022-07-15 17:21:34 +0600

    nested route added

--- src/components/Countries/Countries.js
+++ src/components/Countries/Countries.js
@@ -1,9 +1,34 @@
+// import React, { useEffect, useState } from 'react';
+// import { Link } from 'react-router-dom';
+
+// const Countries = () => {
+//     const [countries, setCountries]= useState([])
+   
+
+//     useEffect(()=>{
+//         fetch("https://restcountries.com/v3.1/all")
+//         .then(res =>res.json())
+//         .then(data=> setCountries(data))
+//     },[])
+//     return (
+//         <div>
+//             <h2>Welcome to my world:{countries.length}</h2>
+//             {/* {
+//                 countries.map(country=> <Link  className='mr-3 underline' to={`/country/:${country.name.common}`} key={country.id}>{country.name.common}</Link>)
+//             } */}
+           
+//         </div>
+//     );
+// };
+
+// export default Countries;
+
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Countries = () => {
-    const [countries, setCountries] = useState([]);
-    
+    const [countries, setCountries] = useState([])
+    // console.log(countries)
 
     useEffect(()=>{
         fetch('https://restcountries.com/v3.1/all')
@@ -12,11 +37,12 @@ const Countries = () => {
     },[])
     return (
         <div>
-            <h2>Welcome to my world:{countries.length}</h2>
-            {
-                countries.map(country=> <Link  className='mr-3 underline' to={`/country/:${country.name.common}`} key={country.id}>{country.name.common}</Link>)
+            <h2>Welcome to the world!{countries.length}</h2>
+           <ul>
+           {
+                countries.map(country =><Link className='mr-3 underline' to={`/country/${country.name.common}`} >{country.name.common}</Link>)
             }
-           
+           </ul>
         </div>
     );
 };
--- src/components/CountryDetail/CountryDetail.js
+++ src/components/CountryDetail/CountryDetail.js
@@ -4,18 +4,33 @@ import { useParams } from 'react-router-dom';
 const CountryDetail = () => {
     const {countryName} =useParams()
 
-    const [country, setCountry] = useState({})
+
+    const [country, setCountry] = useState([])
     console.log(country)
+    
 
     useEffect(()=>{
         fetch(`https://restcountries.com/v3.1/name/${countryName}`)
         .then(res =>res.json())
         .then(data=>setCountry(data))
 
-    },[])
+    },[countryName])
     return (
         <div>
-            <h2>This is for country detail:{countryName}</h2>
+            <h2 className='text-2xl font-bold '>The name of Country: <span className='text-blue-700'>{countryName}</span></h2>
+          <div className=' flex justify-center shadow-lg mt-4'>
+          {
+            country.map(info => 
+            <div>
+              <h1 className="text-xl font-bold "> Official Name : <span className="text-green-600"> {info.name.official}</span></h1>
+              <h1 className="text-xl font-bold">  Capital: {info.capital}</h1>
+              <h1 className="text-xl font-bold">  Population: {info.population}</h1>
+              <h1 className="text-xl font-bold"> Continents: {info.continents}</h1>
+              <img className='w-30 h-30' src={info.flags.png} alt="" />
+
+            </div>)
+           }
+          </div>
             
 
         </div>
